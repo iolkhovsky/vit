53c8c84a8f776d1b12960f7aa4c88610b3619ff0
Igor Olkhovskiy <i.v.olkhovsky@gmail.com>
2023-06-11 09:48:52 +0300

    Add inference testing scripts

--- run.py
+++ run.py
@@ -1,100 +1,95 @@
 import argparse
+import cv2
+import json
 import numpy as np
+import os
 import torch
-import torchvision
 
-from pl_module import VitClassifier
-from dataset import ToTensor
-from utils import (
-    DummyProfiler, save_prediction_report, create_graph_image
-)
+from model import VitClassifier
+from utils import visualize_prediction
+
+
+DEFAULT_CKPT = 'logs/2023-06-1019-42-03-188079/version_0/checkpoints/last.ckpt'
 
 
 def parse_args():
     parser = argparse.ArgumentParser(prog='ViT inference test')
     parser.add_argument(
-        '--checkpoint',
+        '--checkpoint', type=str,
+        default=DEFAULT_CKPT,
         help='Checkpoint path',
     )
     parser.add_argument(
-        '--device', default='cpu',
+        '--device', type=str,
+        default='cpu',
         help='Execution device',
     )
     parser.add_argument(
-        '--batch_size', default=1,
-        help='Batch size',
+        '--source',
+        default=0,
+        help='Video stream source',
     )
     parser.add_argument(
-        '--iterations', default=100,
-        help='Iterations to run',
-    )
-    parser.add_argument(
-        '--report', type=str, default='inference.html',
-        help='Path to export report'
-    )
-    parser.add_argument(
-        '--warmup', type=int, default=10,
-        help='Amount of warmup iterations'
+        '--labels_map', type=str,
+        default='labels.json',
+        help='Path to JSON with labels map',
     )
     return parser.parse_args()
 
 
-def run_inference(args):
-    net = VitClassifier.load_from_checkpoint(args.checkpoint)
+def run(args):
+    net = VitClassifier.load_from_checkpoint(
+        args.checkpoint,
+        map_location=torch.device(args.device)
+    )
     net.freeze()
 
     print(f'Checkpoint:\n{args.checkpoint}')
     print(f'Model:\n{net}')
-    dataset = torchvision.datasets.CIFAR10(
-        root='./data',
-        train=False,
-        download=True,
-        transform=ToTensor(),
-    )
-    loader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=2,
-    )
-    profiler = DummyProfiler('Inference')
-    it = iter(loader)
-    images, descriptions, colors = [], [], []
 
-    for i in range(args.warmup):
-        net(torch.rand(4, 3, 32, 32))
+    labels = None
+    if os.path.isfile(args.labels_map):
+        with open(args.labels_map, 'rt') as f:
+            labels = json.load(f)
+            labels = {int(k): v for k, v in labels.items()}
 
-    for idx in range(args.iterations):
-        imgs, labels = next(it)
-        with profiler:
-            scores, classes = net(imgs)
-        accuracy = (labels == classes).float().mean()
-        print(f'Iteration #{idx} Accuracy: {accuracy}')
+    cap = cv2.VideoCapture(args.source)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-        if args.report:
-            for img, target, prediction, score in zip(imgs, labels, classes, scores):
-                images.append(img.numpy())
-                descriptions.append(
-                    f'Target: {target}\nPrediction: {prediction}\nScore: {score}'
-                )
-                colors.append(
-                    (196, 255, 196) if target == prediction else (255, 196, 196)
-                )
+        frame = cv2.resize(frame, (256, 256))
 
-    if args.report:
-        profiler_graph = create_graph_image(profiler._sample, (100, 300))
-        images.insert(0, profiler_graph)
-        descriptions.insert(0, 'Profiler graph')
-        colors.insert(0, (192, 192, 192))
-
-        save_prediction_report(
-            images=images,
-            descriptions=descriptions,
-            output_file='inference.html',
-            img_size=(128, 128),
-            summary=profiler.summary(),
-            colors=colors,
+        scores, classes, attention = net(
+            np.expand_dims(frame, 0),
+            return_scores=True
         )
-   
+
+        score = scores[0].numpy()
+        score = '{:.3f}'.format(score)
+        class_id = int(classes[0].numpy())
+        if labels:
+            class_id = labels[class_id]
+        attention = attention[0].numpy()
+        description = f'Prediction: {class_id} ({score})'
+
+        attention_vis = visualize_prediction(
+            img=frame,
+            text=description,
+            attention=attention,
+        )
+        
+        print(description)
+    
+        cv2.imshow('Source', frame)
+        cv2.imshow('Attention', attention_vis)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    run_inference(parse_args())
+    run(parse_args())
---
+++ test_inference.py
@@ -0,0 +1,109 @@
+import argparse
+import torch
+import torchvision
+
+from model import VitClassifier
+from dataset import ToTensor
+from utils import (
+    DummyProfiler, save_prediction_report, create_graph_image
+)
+
+
+DEFAULT_CKPT = 'logs/2023-06-1019-42-03-188079/version_0/checkpoints/last.ckpt'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='ViT inference test')
+    parser.add_argument(
+        '--checkpoint', type=str,
+        default=DEFAULT_CKPT,
+        help='Checkpoint path',
+    )
+    parser.add_argument(
+        '--device', type=str,
+        default='cpu',
+        help='Execution device',
+    )
+    parser.add_argument(
+        '--batch_size', type=int,
+        default=1,
+        help='Batch size',
+    )
+    parser.add_argument(
+        '--iterations', type=int,
+        default=100,
+        help='Iterations to run',
+    )
+    parser.add_argument(
+        '--report', type=str, default='inference.html',
+        help='Path to export report'
+    )
+    parser.add_argument(
+        '--warmup', type=int, default=10,
+        help='Amount of warmup iterations'
+    )
+    return parser.parse_args()
+
+
+def run_inference(args):
+    net = VitClassifier.load_from_checkpoint(
+        args.checkpoint,
+        map_location=torch.device(args.device)
+    )
+    net.freeze()
+
+    print(f'Checkpoint:\n{args.checkpoint}')
+    print(f'Model:\n{net}')
+    dataset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=2,
+    )
+    profiler = DummyProfiler('Inference')
+    it = iter(loader)
+    images, descriptions, colors = [], [], []
+
+    for i in range(args.warmup):
+        net(torch.rand(4, 3, 32, 32))
+
+    for idx in range(args.iterations):
+        imgs, labels = next(it)
+        with profiler:
+            scores, classes = net(imgs)
+        accuracy = (labels == classes).float().mean()
+        print(f'Iteration #{idx} Accuracy: {accuracy}')
+
+        if args.report:
+            for img, target, prediction, score in zip(imgs, labels, classes, scores):
+                images.append(img.numpy())
+                descriptions.append(
+                    f'Target: {target}\nPrediction: {prediction}\nScore: {score}'
+                )
+                colors.append(
+                    (196, 255, 196) if target == prediction else (255, 196, 196)
+                )
+
+    if args.report:
+        profiler_graph = create_graph_image(profiler._sample, (100, 300))
+        images.insert(0, profiler_graph)
+        descriptions.insert(0, 'Profiler graph')
+        colors.insert(0, (192, 192, 192))
+
+        save_prediction_report(
+            images=images,
+            descriptions=descriptions,
+            output_file='inference.html',
+            img_size=(128, 128),
+            summary=profiler.summary(),
+            colors=colors,
+        )
+   
+if __name__ == '__main__':
+    run_inference(parse_args())
